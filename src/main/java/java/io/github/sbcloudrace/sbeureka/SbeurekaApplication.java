e9e306cbff9d2613a320140e0948dc621e70fdaf
dazocc <davison.oliveira@dsfnet.com.br>
2020-09-17 22:28:32 -0400

    #sb-eureka inicio

---
+++ src/main/java/java/io/github/sbcloudrace/sbeureka/SbeurekaApplication.java
@@ -0,0 +1,13 @@
+package java.io.github.sbcloudrace.sbeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbeurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbeurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/io/github/sbcloudrace/sbeureka/SbeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package java.io.github.sbcloudrace.sbeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
